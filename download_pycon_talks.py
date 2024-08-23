334fd4aeecce368d39f9c4a1372c021ee406a3fc
Al Sweigart <asweigart@gmail.com>
2024-08-23 11:56:16 -0400

    Updated python scripts for better workflow.

--- download_pycon_talks.py
+++ download_pycon_talks.py
@@ -4,10 +4,8 @@ The transcription by Whisper is a separate step after this script."""
 import json, yt_dlp, os, re, sys
 from pathlib import Path
 
-
 PYCON_EVENTS = sys.argv[1:]
 
-import re
 
 def sanitize_filename(filename: str) -> str:
     filename = filename.replace(' ', '-')
@@ -19,37 +17,40 @@ def download_files_for_event(pycon_event):
     for json_filename in os.listdir(event_path):
         if not json_filename.endswith('.json'): continue  # skip non-json files
 
+        # Get video URLs from pyvideo data folder:
         with open(event_path / json_filename) as event_file:
             event_data = json.loads(event_file.read())
-            event_url = event_data['videos'][0]['url']
-            event_title = sanitize_filename(event_data['title'])
 
-            ytdlp_options = {
-                'quiet': True,
-                'no_warnings': True,
-                'no_progress': True,
-                'format': 'm4a/bestaudio/best',
-                'outtmpl': event_title + '.%(ext)s',
-                'postprocessors': [{  # Extract audio using ffmpeg
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'm4a',
-                }]
-            }
+        event_url = event_data['videos'][0]['url']
+        event_title = sanitize_filename(event_data['title'])
 
-            # Make folder for this event.
-            os.chdir(Path(__file__).parent / 'events')
-            os.makedirs(pycon_event, exist_ok=True)
-            os.chdir(pycon_event)
-            os.makedirs('originals', exist_ok=True)
-            os.makedirs('human-reviewer-notes', exist_ok=True)
+        ytdlp_options = {
+            'quiet': True,
+            'no_warnings': True,
+            'no_progress': True,
+            'format': 'm4a/bestaudio/best',
+            'outtmpl': event_title + '.%(ext)s',
+            'postprocessors': [{  # Extract audio using ffmpeg
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'm4a',
+            }]
+        }
 
-            print(f'Downloading {event_title}...')
+        # Make the folders for this event:
+        os.chdir(Path(__file__).parent / 'events')
+        os.makedirs(pycon_event, exist_ok=True)
+        os.chdir(pycon_event)
+        os.makedirs('originals', exist_ok=True)
+        os.makedirs('human-reviewer-notes', exist_ok=True)
+        os.chdir('originals')  # Videos will go in the "originals" folder.
 
-            try:
-                with yt_dlp.YoutubeDL(ytdlp_options) as ydl:
-                    error_code = ydl.download([event_url])
-            except Exception as e:
-                print(f'ERROR: {e}')
+        # Download the video and extract the audio to a .m4a file:
+        print(f'Downloading {event_title}...')
+        try:
+            with yt_dlp.YoutubeDL(ytdlp_options) as ydl:
+                error_code = ydl.download([event_url])
+        except Exception as e:
+            print(f'ERROR: {e}')
 
 
 for event in PYCON_EVENTS:
--- whisper_pycon_talks.py
+++ whisper_pycon_talks.py
@@ -1,11 +1,26 @@
-import whisper, sys, os
+import whisper, sys, os, shutil
+from pathlib import Path
 
 model = whisper.load_model('medium')
 
+PYCON_EVENTS = sys.argv[1:]
 
-for filename in os.listdir():
-    if not filename.endswith('.m4a'): continue
 
-    result = model.transcribe(filename)
-    whisper.utils.get_writer('srt', '.')(result, 'filename')
-    whisper.utils.get_writer('txt', '.')(result, 'filename')
+def run_whisper_for_event(pycon_event):
+    os.chdir(Path(__file__).parent / f'events/{pycon_event}/originals')
+
+    for filename in os.listdir():
+        if not filename.endswith('.m4a'): continue
+
+        if os.path.exists(filename[:-4] + '.srt'):
+            print(f'Skipping {filename} because it has already been transcribed.')
+
+        print(f'Transcribing {filename}...')
+        result = model.transcribe(filename)
+        whisper.utils.get_writer('srt', '.')(result, 'filename')
+
+    
+
+for event in PYCON_EVENTS:
+    run_whisper_for_event(event)
+    
